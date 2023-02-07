2c0098ebf8a1c4772ffb2f56eac2d18d7c2408f7
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-06 16:32:40 -0800

    update js to feature temp set to farenheit and a weather description.
    Co-authored-by: E Luckie <eluckie.d@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import './css/styles.css';
 
 function getWeather(city) {
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}
-  `;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`
+
+  // const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${zip code},${country code}&appid=${process.env.API_KEY}`; //we need a second js page for this?
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       printElements(response, city);
+    } else {
+      printError(this, response, city);
     }
   });
 
@@ -24,7 +27,13 @@ function getWeather(city) {
 
 function printElements(apiResponse, city) {
   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+  The temperature in Farenheit is ${apiResponse.main.temp} degrees.
+  The conditions will look like ${apiResponse.weather[0].description}.
+  The wind speed is ${apiResponse.wind.speed} mph.`;
+}
+
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText} ${apiResponse.message}`;
 }
 
 function handleFormSubmission(event) {
